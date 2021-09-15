faf916943be8960f05781a3b484337c9d2198dc1
Aqib Nawaz <aqibnawaz1257837@gmail.com>
2021-09-15 08:17:53 +0500

    NetFlix Ui By Aqib in flutter

---
+++ android/app/src/main/kotlin/com/example/netflexuidesignbyaqib/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.netflexuidesignbyaqib
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:netflexuidesignbyaqib/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Netflix UI by AQIb',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
